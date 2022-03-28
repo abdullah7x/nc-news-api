88407223f9d4cc1b8c22d8d054dfd8d787647ca9
abdullah7x <abdullah.ahmed7x@gmail.com>
2022-03-28 15:31:51 +0100

    add further tests for ncnews-6

--- models/models.app.js
+++ models/models.app.js
@@ -1,24 +1,26 @@
 const db = require('../db/connection');
 
-exports.selectAllTopics = () => {
-  return db.query(`SELECT * FROM topics;`).then((results) => {
-    return results.rows;
-  });
+exports.selectAllTopics = async () => {
+  const results = await db.query(`SELECT * FROM topics;`);
+  return results.rows;
 };
 
-exports.editArticleById = (article_id, inc_votes) => {
-  return db
-    .query(
+exports.editArticleById = async (article_id, inc_votes) => {
+  try {
+    const results = await db.query(
       'UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;',
       [inc_votes, article_id]
-    )
-    .then((results) => {
-      if (!results.rows.length) {
-        return Promise.reject({
-          status: 404,
-          message: 'invalid article id',
-        });
-      }
-      return results.rows[0];
-    });
+    );
+    if (!results.rows.length) {
+      return Promise.reject({
+        status: 404,
+        message: 'invalid article id',
+        code: '0000',
+      });
+    } else return results.rows[0];
+  } catch (err) {
+    if ((err.code = '22P02')) {
+      return Promise.reject({ status: 405, message: 'invalid input type' });
+    } else console.log(err);
+  }
 };
