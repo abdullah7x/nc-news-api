66724da7ce7b85f6f34331696ca025530c7e00e1
abdullah7x <abdullah.ahmed7x@gmail.com>
2022-03-28 15:38:49 +0100

    refactor controllers to async await

--- controllers/controllers.app.js
+++ controllers/controllers.app.js
@@ -1,17 +1,17 @@
 const { selectAllTopics, editArticleById } = require('../models/models.app');
 
-exports.getTopics = (req, res) => {
-  selectAllTopics().then((topics) => {
-    res.send(topics);
-  });
+exports.getTopics = async (req, res) => {
+  const topics = await selectAllTopics();
+  res.send(topics);
 };
 
-exports.patchArticle = (req, res, next) => {
+exports.patchArticle = async (req, res, next) => {
   const { inc_votes } = req.body;
   const { article_id } = req.params;
-  editArticleById(article_id, inc_votes)
-    .then((article) => {
-      res.send({ article });
-    })
-    .catch(next);
+  try {
+    const article = await editArticleById(article_id, inc_votes);
+    res.send({ article });
+  } catch (err) {
+    next(err);
+  }
 };
