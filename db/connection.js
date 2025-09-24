988a2ba0648d4fcfd2bb112d72551d5cff553fcd
Abdullah Ahmed <abdullah.ahmed@valtech.com>
2025-09-24 19:45:59 +0100

    update configs to host app

--- db/connection.js
+++ db/connection.js
@@ -8,14 +8,10 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE not set');
 }
-const config =
-  ENV === 'production'
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
-
+const config = {};
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+  
 module.exports = new Pool(config);
